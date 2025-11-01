bd86d3ed14fc6b710816bc7d15c6800d0d3ca0a2
NievaS24 <santinieva24@gmail.com>
2025-11-01 13:06:30 -0300

    Control de Lectura de Productos añadido.

---
+++ Controllers/ProductosController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using tl2_tp7_2025_NievaS24.Models;
+using tl2_tp7_2025_NievaS24.Repository;
+using tl2_tp8_2025_NievaS24.Models;
+
+namespace tl2_tp8_2025_NievaS24.Controllers;
+
+public class ProductosController : Controller
+{
+    private ProductoRepository productoRepository;
+    public ProductosController()
+    {
+        productoRepository = new ProductoRepository();
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        List<Productos> productos = productoRepository.GetAll();
+        return View(productos);
+    }
+
+}
