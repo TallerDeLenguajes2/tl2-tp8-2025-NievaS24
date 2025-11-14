70d51d6f66c60bf23a243d25b3e778329b8d4015
NievaS24 <santinieva24@gmail.com>
2025-11-14 19:26:01 -0300

    Configuración de Servicios en Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,24 @@
+using tl2_tp8_2025_NievaS24.Interface;
+using tl2_tp8_2025_NievaS24.Repository;
+using tl2_tp8_2025_NievaS24.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+// Servicios de Sesión y Acceso a Contexto (CLAVE para la autenticación)
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+// Registro de la Inyección de Dependencia (TODOS AddScoped)
+builder.Services.AddScoped<IProductoRepository, ProductoRepository>();
+builder.Services.AddScoped<IPresupuestoRepository, PresupuestosRepository>();
+builder.Services.AddScoped<IUserRepository, UsuarioRepository>();
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
 
 var app = builder.Build();
 
@@ -12,6 +29,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseSession(); 
 
 app.UseHttpsRedirection();
 app.UseRouting();
