90fc023f8cab462ba7599c33b62be14353215bcf
NievaS24 <santinieva24@gmail.com>
2025-11-14 19:26:21 -0300

    Servicio creado.

---
+++ Services/AuthenticationService.cs
@@ -0,0 +1,27 @@
+using tl2_tp8_2025_NievaS24.Interface;
+namespace tl2_tp8_2025_NievaS24.Services;
+public class AuthenticationService : IAuthenticationService
+{
+    public bool Login(string username, string password)
+    {
+        throw new NotImplementedException();
+    }
+    public void Logout()
+    {
+        throw new NotImplementedException();
+    }
+    public bool IsAuthenticated()
+    {
+        throw new NotImplementedException();
+    }
+    public bool HasAccessLevel(string requiredAccessLevel)
+    {
+        throw new NotImplementedException();
+    }
+
+    
+
+    
+
+    
+}
