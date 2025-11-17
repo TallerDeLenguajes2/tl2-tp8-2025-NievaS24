c71782e19fb6b015a267c08a4befe640a1e7e600
NievaS24 <santinieva24@gmail.com>
2025-11-17 17:36:29 -0300

    Protección de ProductosController realizada.

--- Controllers/ProductosController.cs
+++ Controllers/ProductosController.cs
@@ -8,24 +8,45 @@ namespace tl2_tp8_2025_NievaS24.Controllers;
 
 public class ProductosController : Controller
 {
-    private readonly IProductoRepository productoRepository;
-    private readonly IAuthenticationService servicio;
-    public ProductosController(IProductoRepository productoRepository, IAuthenticationService servicio)
+    private readonly IProductoRepository _productoRepository;
+    private readonly IAuthenticationService _authService;
+    public ProductosController(IProductoRepository productoRepository, IAuthenticationService authService)
     {
-        this.productoRepository = productoRepository;
-        this.servicio = servicio;
+        _productoRepository = productoRepository;
+        _authService = authService;
+    }
+
+    private IActionResult CheckAdminPermissions()
+    {
+        // 1. No logueado? -> vuelve al login
+        if (!_authService.IsAuthenticated())
+        {
+            return RedirectToAction("Index", "Login");
+        }
+
+        // 2. No es Administrador? -> Da Error
+        if (!_authService.HasAccessLevel("Administrador"))
+        {
+            // Llamamos a AccesoDenegado (llama a la vista correspondiente de Productos)
+            return RedirectToAction(nameof(AccesoDenegado));
+        }
+        return null; // Permiso concedido
     }
 
     [HttpGet]
     public IActionResult Index()
     {
-        List<Productos> productos = productoRepository.GetAll();
+        var securityCheck = CheckAdminPermissions();
+        if (securityCheck != null) return securityCheck;
+        List<Productos> productos = _productoRepository.GetAll();
         return View(productos);
     }
 
     [HttpGet]
     public IActionResult Create()
     {
+        var securityCheck = CheckAdminPermissions();
+        if (securityCheck != null) return securityCheck;
         return View();
     }
 
@@ -33,6 +54,8 @@ public class ProductosController : Controller
     [HttpPost]
     public IActionResult Create(ProductoViewModel productoVM)
     {
+        var securityCheck = CheckAdminPermissions();
+        if (securityCheck != null) return securityCheck;
         if (!ModelState.IsValid)
         {
             return View(productoVM);
@@ -42,14 +65,16 @@ public class ProductosController : Controller
             Descripcion = productoVM.Descripcion,
             Precio = productoVM.Precio
         };
-        productoRepository.Create(nuevoProducto);
+        _productoRepository.Create(nuevoProducto);
         return RedirectToAction("Index");
     }
 
     [HttpGet]
     public IActionResult Update(int id)
     {
-        var producto = productoRepository.GetById(id);
+        var securityCheck = CheckAdminPermissions();
+        if (securityCheck != null) return securityCheck;
+        var producto = _productoRepository.GetById(id);
         var productoAEditar = new ProductoViewModel
         {
             idProducto = producto.idProducto,
@@ -63,6 +88,8 @@ public class ProductosController : Controller
     [HttpPost]
     public IActionResult Update(ProductoViewModel productoVM)
     {
+        var securityCheck = CheckAdminPermissions();
+        if (securityCheck != null) return securityCheck;
         if (!ModelState.IsValid)
         {
             return View(productoVM);
@@ -73,21 +100,25 @@ public class ProductosController : Controller
             Descripcion = productoVM.Descripcion,
             Precio = productoVM.Precio
         };
-        productoRepository.Update(productoEditado.idProducto, productoEditado);
+        _productoRepository.Update(productoEditado.idProducto, productoEditado);
         return RedirectToAction("Index");
     }
 
     [HttpGet]
     public IActionResult Delete(int id)
     {
-        Productos prodABorrar = productoRepository.GetById(id);
+        var securityCheck = CheckAdminPermissions();
+        if (securityCheck != null) return securityCheck;
+        Productos prodABorrar = _productoRepository.GetById(id);
         return View(prodABorrar);
     }
 
     [HttpPost]
     public IActionResult DeleteConfirmed(int idProducto)
     {
-        productoRepository.Delete(idProducto);
+        var securityCheck = CheckAdminPermissions();
+        if (securityCheck != null) return securityCheck;
+        _productoRepository.Delete(idProducto);
         return RedirectToAction("Index");
     }
 
